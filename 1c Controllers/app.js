768885d43bb0cbf6857cd8bc307c70f3ae5472fb
fbozas <fbozas@hotmail.com>
2023-09-01 12:33:32 +0300

    Controllers in AngularJS

---
+++ 1c Controllers/app.js
@@ -0,0 +1,11 @@
+var myApp = angular.module("myModule", []);
+
+myApp.controller("myController", function($scope){
+    var employee = {
+        firstName: "David",
+        lastName: "Hastings",
+        gender: "Male"
+    };
+
+    $scope.employee = employee;
+});
